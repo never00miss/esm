544578f1b78d35efd4c77144524c1e158d6221ed
Allandhino Pattras <allan.putra@yahoo.com>
2024-03-07 20:35:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* eslint strict: off, node/no-unsupported-features: ["error", { version: 6 }] */
+/* eslint strict: off, node/no-unsupported-features: ["error", { version: 18 }] */
 "use strict"
 
 module.exports = require("./esm.js")
